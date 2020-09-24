282c6a6c01469022f6fedf62459c81aa6ee11022
Nataly <nataly-wien@yandex.ru>
2020-09-24 22:28:47 +0200

    Исправляет недочеты (настройка персонажей)

--- js/setup.js
+++ js/setup.js
@@ -1,56 +1,95 @@
 'use strict';
 
-const FIRST_NAMES_LIST = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const NAMES_LIST = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const COAT_COLORS_LIST = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-const EYES_COLORS_LIST = [`black`, `red`, `blue`, `yellow`, `green`];
+const FIRST_NAMES_LIST = [
+  `Иван`,
+  `Хуан Себастьян`,
+  `Мария`,
+  `Кристоф`,
+  `Виктор`,
+  `Юлия`,
+  `Люпита`,
+  `Вашингтон`,
+];
 
-const VIZARDS_NUMBER = 4;
+const NAMES_LIST = [
+  `да Марья`,
+  `Верон`,
+  `Мирабелла`,
+  `Вальц`,
+  `Онопко`,
+  `Топольницкая`,
+  `Нионго`,
+  `Ирвинг`,
+];
+
+const COAT_COLORS_LIST = [
+  `rgb(101, 137, 164)`,
+  `rgb(241, 43, 107)`,
+  `rgb(146, 100, 161)`,
+  `rgb(56, 159, 117)`,
+  `rgb(215, 210, 55)`,
+  `rgb(0, 0, 0)`,
+];
+
+const EYES_COLORS_LIST = [
+  `black`,
+  `red`,
+  `blue`,
+  `yellow`,
+  `green`,
+];
+
+const WIZARDS_NUMBER = 4;
+
+const gameSetup = document.querySelector(`.setup`);
+const similarWizardsList = gameSetup.querySelector(`.setup-similar-list`);
+const wizardTemplate = document.querySelector(`#similar-wizard-template`)
+  .content
+  .querySelector(`.setup-similar-item`);
+
 
 const getRandomInRange = (range) => Math.floor(Math.random() * range);
 
 const getRandomFromList = (list) => list[getRandomInRange(list.length)];
 
-const getWizardsList = function () {
-  let wizardsList = [];
-  for (let i = 0; i < VIZARDS_NUMBER; i++) {
+const getWizardsList = () => {
+  const wizardsList = [];
+
+  for (let i = 0; i < WIZARDS_NUMBER; i++) {
     const firstName = getRandomFromList(FIRST_NAMES_LIST);
     const name = getRandomFromList(NAMES_LIST);
-    const wizard = {
-      name: getRandomInRange(2) ? `${firstName} ${name}` : `${name} ${firstName}`,
+
+    wizardsList.push({
+      name: Math.random() > 0.5 ? `${firstName} ${name}` : `${name} ${firstName}`,
       coatColor: getRandomFromList(COAT_COLORS_LIST),
-      eyesColor: getRandomFromList(EYES_COLORS_LIST)
-    };
-    wizardsList.push(wizard);
+      eyesColor: getRandomFromList(EYES_COLORS_LIST),
+    });
   }
+
   return wizardsList;
 };
 
-const getWizardsBlock = function () {
+const generateWizard = (wizard) => {
+  const wizardItem = wizardTemplate.cloneNode(true);
+  wizardItem.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardItem.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardItem.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
 
-  const getWizardExemplar = function (template, wizard) {
-    const wizardExemplar = template.cloneNode(true);
-    wizardExemplar.querySelector(`.setup-similar-label`).textContent = wizard.name;
-    wizardExemplar.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-    wizardExemplar.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
-    return wizardExemplar;
-  };
+  return wizardItem;
+};
 
-  let fragment = document.createDocumentFragment();
-  const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+const getWizardsBlock = () => {
   const wizards = getWizardsList();
+  const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < VIZARDS_NUMBER; i++) {
-    fragment.appendChild(getWizardExemplar(wizardTemplate, wizards[i]));
+  for (let i = 0; i < WIZARDS_NUMBER; i++) {
+    fragment.appendChild(generateWizard(wizards[i]));
   }
+
   return fragment;
 };
 
 
-const gameSetup = document.querySelector(`.setup`);
 gameSetup.classList.remove(`hidden`);
-
-const similarWizardsList = gameSetup.querySelector(`.setup-similar-list`);
 similarWizardsList.appendChild(getWizardsBlock());
-
 gameSetup.querySelector(`.setup-similar`).classList.remove(`hidden`);
