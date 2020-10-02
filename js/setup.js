d7c87db98fc3918723b54e163209429e2c79c765
Nataly <nataly-wien@yandex.ru>
2020-10-02 16:03:53 +0200

    Добавляет обработку нажатий и валидацию ввода имени

--- js/setup.js
+++ js/setup.js
@@ -39,14 +39,41 @@ const EYES_COLORS_LIST = [
   `green`,
 ];
 
+const FIREBALL_COLORS_LIST = [
+  `#ee4830`,
+  `#30a8ee`,
+  `#5ce6c0`,
+  `#e848d5`,
+  `#e6e848`,
+];
+
 const WIZARDS_NUMBER = 4;
+const UESRNAME_MINLENGTH = 2;
+const UESRNAME_MAXLENGTH = 25;
+
+const usernameValidationMessages = [
+  `Введите имя персонажа`,
+  `Имя персонажа должно состоять минимум из ${UESRNAME_MINLENGTH}-х символов`,
+  `Максимальная длина имени персонажа - ${UESRNAME_MAXLENGTH} символов`,
+  `Введите еще`,
+  `Удалите лишние`,
+];
 
 const gameSetup = document.querySelector(`.setup`);
 const similarWizardsList = gameSetup.querySelector(`.setup-similar-list`);
 const wizardTemplate = document.querySelector(`#similar-wizard-template`)
   .content
   .querySelector(`.setup-similar-item`);
-
+const openSetupBlock = document.querySelector(`.setup-open`);
+const closeButton = document.querySelector(`.setup-close`);
+const usernameField = document.querySelector(`.setup-user-name`);
+const wizardCoat = document.querySelector(`.setup-wizard .wizard-coat`);
+const wizardCoatField = document.querySelector(`input[name="coat-color"]`);
+const wizardEyes = document.querySelector(`.setup-wizard .wizard-eyes`);
+const wizardEyesField = document.querySelector(`input[name="eyes-color"]`);
+const fireBall = document.querySelector(`.setup-fireball-wrap`);
+const fireballField = document.querySelector(`input[name="fireball-color"]`);
+const setupPlayer = document.querySelector(`.setup-player`);
 
 const getRandomInRange = (range) => Math.floor(Math.random() * range);
 
@@ -90,6 +117,101 @@ const getWizardsBlock = () => {
 };
 
 
-gameSetup.classList.remove(`hidden`);
+const openSetup = () => {
+  gameSetup.classList.remove(`hidden`);
+  window.addEventListener(`keydown`, onWindowClick);
+  closeButton.addEventListener(`click`, onCloseButtonClick);
+  closeButton.addEventListener(`keydown`, onCloseButtonDown);
+  usernameField.addEventListener(`invalid`, onUsernameFieldInvalid);
+  usernameField.addEventListener(`input`, onUsernameFieldInput);
+  setupPlayer.addEventListener(`click`, onPlayerClick);
+};
+
+openSetupBlock.addEventListener(`click`, () => openSetup());
+
+openSetupBlock.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    openSetup();
+  }
+});
+
+const closeSetup = () => {
+  gameSetup.classList.add(`hidden`);
+  window.removeEventListener(`keydown`, onWindowClick);
+  closeButton.removeEventListener(`click`, onCloseButtonClick);
+  closeButton.removeEventListener(`keydown`, onCloseButtonDown);
+  usernameField.removeEventListener(`invalid`, onUsernameFieldInvalid);
+  usernameField.removeEventListener(`input`, onUsernameFieldInput);
+  setupPlayer.removeEventListener(`click`, onPlayerClick);
+};
+
+const onWindowClick = (evt) => {
+  if (evt.key === `Escape` && usernameField !== document.activeElement) {
+    evt.preventDefault();
+    closeSetup();
+  }
+};
+
+const onCloseButtonClick = () => closeSetup();
+
+const onCloseButtonDown = (evt) => {
+  if (evt.key === `Enter`) {
+    closeSetup();
+  }
+};
+
+const onUsernameFieldInvalid = () => {
+  switch (true) {
+    case usernameField.validity.valueMissing:
+      usernameField.setCustomValidity(usernameValidationMessages[0]);
+      break;
+    case usernameField.validity.tooShort:
+      usernameField.setCustomValidity(usernameValidationMessages[1]);
+      break;
+    case usernameField.validity.tooLong:
+      usernameField.setCustomValidity(usernameValidationMessages[2]);
+      break;
+    default:
+      usernameField.setCustomValidity(``);
+  }
+};
+
+const getWordForm = (number, forms) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  number = Math.floor(Math.abs(number)) % 100;
+
+  return forms[number > 4 && number < 20 ? 2 : cases[Math.min(number % 10, 5)]];
+};
+
+const onUsernameFieldInput = () => {
+  const fieldLength = usernameField.value.length;
+
+  if (fieldLength < UESRNAME_MINLENGTH) {
+    usernameField.setCustomValidity(`${usernameValidationMessages[3]} ${UESRNAME_MINLENGTH - fieldLength} ${getWordForm(UESRNAME_MINLENGTH - fieldLength, [`символ`, `символа`, `символов`])}`);
+  } else if (fieldLength > UESRNAME_MAXLENGTH) {
+    usernameField.setCustomValidity(`${usernameValidationMessages[4]} ${fieldLength - UESRNAME_MAXLENGTH} ${getWordForm(fieldLength - UESRNAME_MAXLENGTH, [`символ`, `символа`, `символов`])}`);
+  } else {
+    usernameField.setCustomValidity(``);
+  }
+};
+
+const onPlayerClick = (evt) => {
+  switch (evt.target) {
+    case document.querySelector(`.setup-fireball`):
+      fireballField.value = getRandomFromList(FIREBALL_COLORS_LIST);
+      fireBall.style.backgroundColor = fireballField.value;
+      break;
+    case wizardEyes:
+      wizardEyesField.value = getRandomFromList(EYES_COLORS_LIST);
+      wizardEyes.style.fill = wizardEyesField.value;
+      break;
+    case wizardCoat:
+      wizardCoatField.value = getRandomFromList(COAT_COLORS_LIST);
+      wizardCoat.style.fill = wizardCoatField.value;
+      break;
+  }
+};
+
+
 similarWizardsList.appendChild(getWizardsBlock());
 gameSetup.querySelector(`.setup-similar`).classList.remove(`hidden`);
